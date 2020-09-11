7413a97917b6cab4077efb5007d95b923d1c62d6
xulei <18039671980@163.com>
2020-09-11 14:42:30 +0800

    引入覆盖vantUI样式目录

--- vue.config.js
+++ vue.config.js
@@ -53,6 +53,8 @@ module.exports = {
     }
   },
   css: {
+    extract: IS_PROD, // 是否将组件中的 CSS 提取至一个独立的 CSS 文件中 (而不是动态注入到 JavaScript 中的 inline 代码)。
+    sourceMap: false,
     loaderOptions: {
       postcss: {
         plugins: [
@@ -63,9 +65,17 @@ module.exports = {
             propList: ['*', '!font*']
           })
         ]
+      },
+      less: {
+        lessOptions: {
+          modifyVars: {
+            hack: `true; @import "@/styles/index.less";`
+          }
+        }
       }
     }
   },
+
   chainWebpack: config => {
     config.plugins.delete('preload') // TODO: need test
     config.plugins.delete('prefetch') // TODO: need test
